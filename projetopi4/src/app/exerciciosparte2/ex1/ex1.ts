7ac9bfd8129ebae5ec8be68126c0dfe84186a72f
RafaelAmadei <orafaelamadei@gmail.com>
2026-09-15 23:39:55 +0000

    commit

---
+++ projetopi4/src/app/exerciciosparte2/ex1/ex1.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex1',
+  imports: [],
+  templateUrl: './ex1.html',
+  styleUrl: './ex1.css',
+})
+export class Ex1 {mensagemVisivel: boolean = false;
+
+  alternarMensagem(): void {
+    this.mensagemVisivel = !this.mensagemVisivel;}}
